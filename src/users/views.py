0717a93512063be8ee3f92dd3e195b3f683ad9aa
MICHAŁ <mg814@wp.pl>
2023-07-25 16:20:59 +0200

    feat: added success message in RegisterView

--- src/users/views.py
+++ src/users/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import UserRegisterForm, LoginForm
 
 
-class RegisterView(CreateView):
+class RegisterView(SuccessMessageMixin, CreateView):
     template_name = 'users/register.html'
     form_class = UserRegisterForm
     success_url = reverse_lazy('login')
+    success_message = "You have been successfully signed up!"
 
 
 class CustomLoginView(LoginView):
