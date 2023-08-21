dea135e53affcbff18039d5904e0ea439b0fabcf
MICHAŁ <mg814@wp.pl>
2023-08-21 11:31:02 +0200

    feat: added products user view

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, ProductCreateView, DetailPageView
+from .views import HomePageView, ProductCreateView, DetailPageView, UserProductsPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('product/add/', ProductCreateView.as_view(), name='product-add'),
     path('product/details/<int:pk>/', DetailPageView.as_view(), name='product-detail'),
+    path('product/my-products', UserProductsPageView.as_view(), name='product-user'),
 ]
