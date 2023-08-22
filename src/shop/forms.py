1ceb5c23076de1ab635d5ca91693514e70c2a4bd
MICHAŁ <mg814@wp.pl>
2023-08-22 12:21:15 +0200

    feat: added product editing

--- src/shop/forms.py
+++ src/shop/forms.py
@@ -26,3 +26,27 @@ class ProductForm(ModelForm):
 
 
         return instance
+
+
+class EditForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'price']
+
+    files = MultiFileField(min_num=1, max_num=3, max_file_size=1024 * 1024 * 5)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs.update({"class": "form-control"})
+        self.fields["description"].widget.attrs.update({"class": "form-control"})
+        self.fields["price"].widget.attrs.update({"class": "form-control"})
+        self.fields["files"].widget.attrs.update({"class": "form-control-file"})
+        self.fields["files"].required = False
+
+    def save(self, *args, **kwargs):
+        instance = super(EditForm, self).save()
+        for each in self.cleaned_data['files']:
+            Image.objects.create(product=instance, image=each)
+
+        return instance
--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import HomePageView, ProductCreateView, DetailPageView, UserProductsPageView
+from .views import HomePageView, ProductCreateView, DetailPageView, UserProductsPageView, EditProductView, \
+    DeleteImageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('product/add/', ProductCreateView.as_view(), name='product-add'),
     path('product/details/<int:pk>/', DetailPageView.as_view(), name='product-detail'),
     path('product/my-products', UserProductsPageView.as_view(), name='product-user'),
+    path('product/edit/<int:pk>/', EditProductView.as_view(), name='product-edit'),
+    path('product/<int:pk>/delete-image/<int:img_id>/', DeleteImageView.as_view(), name='product-image-delete'),
 ]
