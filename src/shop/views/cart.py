67fc900fd1e3dbafc3d8b38f2fa7204f6933aa3f
MICHAŁ <mg814@wp.pl>
2024-01-31 15:44:05 +0100

    refactor: functions changed to class

--- src/shop/views/cart.py
+++ src/shop/views/cart.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 
 from shop.models import Product, Cart, Item
 from shop.mixins import ExtraContextMixin
@@ -42,44 +42,41 @@ class CartHomePageView(ExtraContextMixin, ListView):
         return context
 
 
-class DeleteCartProductView(View):
-    def get(self, request, pk):
-        product_id = Product.objects.filter(id=pk).values('id')[0].get('id')
-        item_in_cart = Item.objects.filter(product_id=product_id)
-
-        item_in_cart.delete()
-
-        return redirect('user-cart')
+class DeleteCartProductView(DeleteView):
+    model = Item
+    success_url = reverse_lazy("user-cart")
 
 
-def add_to_cart(request):
-    if request.method == 'POST':
+class AddToCart(View):
+    def post(self, request):
         user = request.user.id
-        product_id_from = request.POST.get('product_id')
-        cart_id = Cart.objects.filter(user_id=user).values('id')[0].get('id')
-        product_id = Product.objects.filter(id=product_id_from).values('id')[0].get('id')
+        product_id_from = request.POST.get("product_id")
+        cart_id = Cart.objects.filter(user_id=user).values("id")[0].get("id")
+        product_id = (
+            Product.objects.filter(id=product_id_from).values("id")[0].get("id")
+        )
 
-        product_id_query_set = Item.objects.filter(cart_id=cart_id).values('product_id')
-        product_id_list = [item.get('product_id') for item in product_id_query_set]
+        product_id_query_set = Item.objects.filter(cart_id=cart_id).values("product_id")
+        product_id_list = [item.get("product_id") for item in product_id_query_set]
 
         if product_id not in product_id_list:
             new_item_in_cart = Item(product_id=product_id, cart_id=cart_id)
             new_item_in_cart.save()
 
-        return redirect('user-cart')
+        return redirect("user-cart")
 
 
-def change_quantity(request):
-    if request.method == 'POST':
-        product_id_from = request.POST.get('product_id')
-        operation = request.POST.get('operation')
+class ChangeQuantity(View):
+    def post(self, request):
+        product_id_from = request.POST.get("product_id")
+        operation = request.POST.get("operation")
 
         item_all = Item.objects.get(id=product_id_from)
-        if operation == 'plus':
+        if operation == "plus":
             item_all.quantity += 1
-        elif operation == 'minus':
+        elif operation == "minus":
             if item_all.quantity > 1:
                 item_all.quantity -= 1
         item_all.save()
 
-        return redirect('user-cart')
+        return redirect("user-cart")
