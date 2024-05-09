6f653d39cb99f67cc6d7669eb6d263e745af5185
MICHAŁ <mg814@wp.pl>
2024-05-09 20:03:34 +0200

    feat: add defoult variable for USE_S3

--- src/core/env.py
+++ src/core/env.py
@@ -3,5 +3,6 @@ import environ
 
 env = environ.Env(
     DEBUG=(bool, False),
-    SECRET_KEY=(str, get_random_secret_key())
+    SECRET_KEY=(str, get_random_secret_key()),
+    USE_S3=(bool, False)
 )
